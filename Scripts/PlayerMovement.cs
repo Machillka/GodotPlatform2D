37e9f38a31fb86ee5b8c79f56aa414dd9fddb935
Machillka <machillka2007@gmail.com>
2025-04-11 15:58:35 +0800

    first commit

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class PlayerMovement : Sprite2D
+{
+    // public void Test() => {GD.Print("Hello, world!");}
+    private float _rotationSpeed = 1.0f;
+    private float _movementSpeed = 500f;
+    private int _direction = 0;
+
+    public override void _Process(double delta)
+    {
+        // Rotation += (float)delta * _rotationSpeed;
+
+        if (Input.IsActionPressed("ui_left"))
+        {
+            _direction = -1;
+        }
+        if (Input.IsActionPressed("ui_right"))
+        {
+            _direction = 1;
+        }
+
+        // 左右移动
+        Position += new Vector2(_direction * _movementSpeed * (float)delta, 0);
+    }
+
+
+}
